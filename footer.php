eb92cab866d27f9117fa35a93d03ffc488b00a24
Brandon Bland <brandonbland90@gmail.com>
2023-11-15 14:16:01 -0500

    Add conditional rendering of footer menus

--- footer.php
+++ footer.php
@@ -4,32 +4,44 @@
         <div class="row mb-3">
           <!-- Start Footer Menu -->
           <div class="col-md-4 mb-md-3 text-center">
-          <?php wp_nav_menu(array(
-            'theme_location' => 'footer_menu_location_left',
-            'menu_class' => 'small mb-3 mb-md-0',
-            'depth' => '1',
-            )); ?>
+            <?php
+            if (has_nav_menu('footer_menu_location_left')) :
+              wp_nav_menu(array(
+                'theme_location' => 'footer_menu_location_left',
+                'menu_class' => 'small mb-3 mb-md-0',
+                'depth' => '1',
+              ));
+            endif;
+            ?>
           </div>
           <!-- End Footer Menu -->
           <!-- Start Footer Menu -->
           <div class="col-md-4 mb-md-3 text-center">
-            <?php wp_nav_menu(array(
-              'theme_location' => 'footer_menu_location_middle',
-              'menu_class' => 'small mb-3 mb-md-0',
-              'depth' => '1',
-            )); ?>
+            <?php
+            if (has_nav_menu('footer_menu_location_middle')) :
+              wp_nav_menu(array(
+                'theme_location' => 'footer_menu_location_middle',
+                'menu_class' => 'small mb-3 mb-md-0',
+                'depth' => '1',
+              ));
+            endif;
+            ?>
           </div>
           <!-- End Footer Menu -->
           <!-- Start Footer Menu -->
           <div class="col-md-4 mb-md-3 text-center">
             <div class="col-md-4 mb-md-3 text-center">
-            <?php wp_nav_menu(array(
-              'theme_location' => 'footer_menu_location_right',
-              'menu_class' => 'small mb-3 mb-md-0',
-              'depth' => '1',
-            )); ?>
-          </div>
-          <!-- footer_menu_location_right -->
+              <?php
+              if (has_nav_menu('footer_menu_location_right')) :
+                wp_nav_menu(array(
+                  'theme_location' => 'footer_menu_location_right',
+                  'menu_class' => 'small mb-3 mb-md-0',
+                  'depth' => '1',
+                ));
+              endif;
+              ?>
+            </div>
+            <!-- footer_menu_location_right -->
           </div>
           <!-- End Footer Menu -->
         </div>
@@ -41,5 +53,6 @@
     <!-- End Footer -->
 
     <?php wp_footer(); ?>
-  </body>
-</html>
+    </body>
+
+    </html>
